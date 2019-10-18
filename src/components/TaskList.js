f8fd18a30b1600a2ed6deda871b5a5158cb518dc
Luis Vilchez <dev11@codecraftdev.com>
2019-10-18 16:49:39 -0400

    add inbox screen and redux mock store to storybook

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
 import Task from "./Task";
 
-function TaskList(props) {
+export function PureTaskList(props) {
   const { loading, tasks, onPinTask, onArchiveTask } = props;
 
   const events = {
@@ -59,15 +61,25 @@ function TaskList(props) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   onPinTask: PropTypes.func.isRequired,
   onArchiveTask: PropTypes.func.isRequired
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false
 };
 
-export default TaskList;
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      t => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    )
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id))
+  })
+)(PureTaskList);
